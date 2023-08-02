a0e6354f8459740fc17f4164a343631e845754ce
AlexanderOI <vialexoriba1257@gmail.com>
2023-08-02 17:27:50 -0400

    The page was scraped to obtain data

---
+++ remove.js
@@ -0,0 +1,9 @@
+const marca = document.querySelectorAll('.product-brand');
+marca.forEach(element => {
+  element.remove();
+});
+
+const descuento = document.querySelectorAll('.oldproductPrice');
+descuento.forEach(element => {
+  element.remove();
+});
---
+++ scraping.py
@@ -0,0 +1,53 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+# product-title-link
+# productPrice
+# picture-link
+
+url = 'https://www.stock.com.py/default.aspx/'
+directory_path_img = 'images/'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    titles = soup.find_all('a', class_='product-title-link')
+
+    prices = soup.find_all('span', class_='productPrice')
+
+    a_images = soup.find_all('a', class_='picture-link')
+
+    count = 0
+
+    for title, price, a_image in zip(titles, prices, a_images):
+        print(title.text)
+        print(price.text)
+
+        images = a_image.find_all('img')
+
+        count += 1
+        for idx, image in enumerate(images):
+            image_url = image.get('src')
+            image_alt = image.get('alt')
+            print(image_alt)
+
+            if image_url:
+                image_response = requests.get(image_url)
+
+                if image_response.status_code == 200:
+                    image_path = os.path.join(
+                        directory_path_img, f"ProductoId - {count}.jpg")
+                    with open(image_path, 'wb') as file:
+                        file.write(image_response.content)
+                else:
+                    print(
+                        f"Error al descargar la imagen {idx}. Código de estado: {image_response.status_code}")
+
+else:
+    print(
+        f"Error al obtener la página. Código de estado: {response.status_code}")
