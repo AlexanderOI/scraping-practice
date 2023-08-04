7903500979874a72126528ff9da74f4c1cd9df7a
AlexanderOI <vialexoriba1257@gmail.com>
2023-08-04 18:46:05 -0400

    Fixed the alts = None error and the URLs error

--- scraping.py
+++ scraping.py
@@ -23,21 +23,36 @@ def get_products(url, id_num):
 
         a_images = soup.find_all('a', class_='picture-link')
 
-        id_num += 1
+        if titles == []:
+            with open('data/products.txt', 'a', encoding='utf-8') as file:
+                file.write(' \n')
+            return False, id_num
+
         for title, price, a_image in zip(titles, prices, a_images):
-            print(title.text)
-            print(price.text)
+            title.text
+            price.text
 
             images = a_image.find_all('img')
 
             for image in images:
+
                 image_url = image.get('src')
                 image_alt = image.get('alt')
+                with open('data/products.txt', 'a', encoding='utf-8') as file:
+                    if image_alt is None:
+                        image_alt = 'None'
+
+                    file.write(title.text + '\n')
+                    file.write(image_alt + '\n')
+                    file.write(price.text + '\n')
+
+                print(title.text)
                 print(image_alt)
+                print(price.text)
 
                 if image_url:
                     image_response = requests.get(image_url)
-
+                    id_num += 1
                     if image_response.status_code == 200:
                         image_path = os.path.join(
                             directory_path_img, f"ProductoId - {id_num}.jpg")
@@ -47,24 +62,32 @@ def get_products(url, id_num):
                         print(
                             f"Error al descargar la imagen {id_num}. Código de estado: {image_response.status_code}")
 
-        return True
+        return True, id_num
 
     else:
         print(
             f"Error al obtener la página. Código de estado: {response.status_code}")
-        return False
+        with open('data/products.txt', 'a', encoding='utf-8') as file:
+            file.write(' \n')
+        return False, id_num
 
 
 extent_url_pages = '?pageindex='
-space = " \n"
+space = ' \n'
 
-with open('data/URL_products.txt', 'r', encoding='uft-8') as file:
+with open('data/URL_products.txt', 'r', encoding='utf-8') as file:
     URL_products = file.readlines()
 
-pages = True
 id_num = 0
 for url in URL_products:
     count = 0
+    pages = True
     if url == space:
+        with open('data/products.txt', 'a', encoding='utf-8') as file:
+            file.write(space)
+    else:
         while pages:
-            pages = get_products(f'{url}{extent_url_pages}{count}', id_num)
+            url_op = url.replace('\n', '')
+            count += 1
+            pages, id_num = get_products(
+                f'{url_op}{extent_url_pages}{count}', id_num)
