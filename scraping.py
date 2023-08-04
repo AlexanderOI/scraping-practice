458025d1af17b714cca51d148f599ae9135e694c
AlexanderOI <vialexoriba1257@gmail.com>
2023-08-04 11:01:59 -0400

    Scraping prepared for data collection

--- scraping.py
+++ scraping.py
@@ -2,52 +2,69 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-
 # product-title-link
 # productPrice
-# picture-link
+# picture-link ?pageindex=2
+# url-pages: 'https://www.stock.com.py/default.aspx?pageindex=1'
 
-url = 'https://www.stock.com.py/default.aspx/'
 directory_path_img = 'images/'
 
-response = requests.get(url)
 
-if response.status_code == 200:
+def get_products(url, id_num):
+    response = requests.get(url)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    if response.status_code == 200:
 
-    titles = soup.find_all('a', class_='product-title-link')
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    prices = soup.find_all('span', class_='productPrice')
+        titles = soup.find_all('a', class_='product-title-link')
 
-    a_images = soup.find_all('a', class_='picture-link')
+        prices = soup.find_all('span', class_='productPrice')
 
+        a_images = soup.find_all('a', class_='picture-link')
+
+        id_num += 1
+        for title, price, a_image in zip(titles, prices, a_images):
+            print(title.text)
+            print(price.text)
+
+            images = a_image.find_all('img')
+
+            for image in images:
+                image_url = image.get('src')
+                image_alt = image.get('alt')
+                print(image_alt)
+
+                if image_url:
+                    image_response = requests.get(image_url)
+
+                    if image_response.status_code == 200:
+                        image_path = os.path.join(
+                            directory_path_img, f"ProductoId - {id_num}.jpg")
+                        with open(image_path, 'wb') as file:
+                            file.write(image_response.content)
+                    else:
+                        print(
+                            f"Error al descargar la imagen {id_num}. Código de estado: {image_response.status_code}")
+
+        return True
+
+    else:
+        print(
+            f"Error al obtener la página. Código de estado: {response.status_code}")
+        return False
+
+
+extent_url_pages = '?pageindex='
+space = " \n"
+
+with open('data/URL_products.txt', 'r', encoding='uft-8') as file:
+    URL_products = file.readlines()
+
+pages = True
+id_num = 0
+for url in URL_products:
     count = 0
-
-    for title, price, a_image in zip(titles, prices, a_images):
-        print(title.text)
-        print(price.text)
-
-        images = a_image.find_all('img')
-
-        count += 1
-        for image in images:
-            image_url = image.get('src')
-            image_alt = image.get('alt')
-            print(image_alt)
-
-            if image_url:
-                image_response = requests.get(image_url)
-
-                if image_response.status_code == 200:
-                    image_path = os.path.join(
-                        directory_path_img, f"ProductoId - {count}.jpg")
-                    with open(image_path, 'wb') as file:
-                        file.write(image_response.content)
-                else:
-                    print(
-                        f"Error al descargar la imagen {count}. Código de estado: {image_response.status_code}")
-
-else:
-    print(
-        f"Error al obtener la página. Código de estado: {response.status_code}")
+    if url == space:
+        while pages:
+            pages = get_products(f'{url}{extent_url_pages}{count}', id_num)
